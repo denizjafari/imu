6097165440ee6557b8d0dcae15feb6137b7f6057
Alex Pasat <123707273+alex-pasat@users.noreply.github.com>
2024-09-29 00:33:17 -0400

    Update filters.py

--- testing/filters.py
+++ testing/filters.py
@@ -1,6 +1,7 @@
 #This file contains filters that can be used to reduce noise generated in the sensors
 
 from scipy.signal import butter, lfilter
+import numpy as np
 
 def low_pass_filter(data, cutoff, fs, order=4):
     nyquist = 0.5 * fs
@@ -13,3 +14,7 @@ def high_pass_filter(data, cutoff, fs, order=4):
     normal_cutoff = cutoff / nyquist
     b, a = butter(order, normal_cutoff, btype='high', analog=False)
     return lfilter(b, a, data)
+
+def clip_data(data):
+    return np.clip(data, -1, 1)  # Clip values between 0 and 1
+
